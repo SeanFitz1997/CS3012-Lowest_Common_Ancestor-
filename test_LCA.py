0000ab1dd59b3ac57d021f1e880a9439120fb303
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-09-21 15:55:03 +0100

    Fixing initial tests (Used wrong assert methods used)

--- test_LCA.py
+++ test_LCA.py
@@ -5,14 +5,14 @@ class test_LCA(unittest.TestCase):
 
     def test_findLCA(self):
         #Test 1: test all values equal None
-        self.assertAlmostEqual(LCA.findLCA(None, None, None), None)
+        self.assertEqual(LCA.findLCA(None, None, None), None)
 
         #Test 2: test when both node are root
         root = LCA.Node(1)
-        self.assertAlmostEqual(LCA.findLCA(root, 1, 1), 1)
+        self.assertEqual(LCA.findLCA(root, 1, 1), 1)
 
         #Test 3: test when a value is not in tree
-        self.assertSequenceEqual(LCA.findLCA(root, 50, 51), None)
+        self.assertEqual(LCA.findLCA(root, 50, 51), None)
 
         #Test 4: LCA 4-5
         root.left = LCA.Node(2) 
@@ -21,16 +21,16 @@ class test_LCA(unittest.TestCase):
         root.left.right = LCA.Node(5) 
         root.right.left = LCA.Node(6) 
         root.right.right = LCA.Node(7)
-        self.assertSequenceEqual(LCA.findLCA(root, 4, 5), 2)
+        self.assertEqual(LCA.findLCA(root, 4, 5), 2)
 
         #Test 5: LCA 4-6
-        self.assertSequenceEqual(LCA.findLCA(root, 4, 6), 1)
+        self.assertEqual(LCA.findLCA(root, 4, 6), 1)
 
         #Test 6: LCA 3-4
-        self.assertSequenceEqual(LCA.findLCA(root, 3, 4), 1)
+        self.assertEqual(LCA.findLCA(root, 3, 4), 1)
 
         #Test 7: LCA 2-4
-        self.assertSequenceEqual(LCA.findLCA(root, 2, 4), 2)
+        self.assertEqual(LCA.findLCA(root, 2, 4), 2)
 
 if(__name__ == '__main__'):
     unittest.main()
