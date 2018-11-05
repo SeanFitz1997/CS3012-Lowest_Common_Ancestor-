eb1ca08482c20069739bce0d65f7a568ec0479b4
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-11-05 14:32:22 +0000

    Created test file and wrote initial tests for getProjects function

---
+++ Github_Access/test_Access.py
@@ -0,0 +1,14 @@
+import unittest
+import Access
+
+class test_LCA(unittest.TestCase):
+
+    def test_getProjects(self):
+        #Test 1: Test fuction with None value
+        self.assertEqual(Access.getProjects(None), 'No User provided')
+
+        #Test 2: Test with valid user
+        # TODO test with my account
+
+if(__name__ == '__main__'):
+    unittest.main()
