0d92b1d9f9d41d80335693684d2cafa11cb56512
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-11-05 14:36:14 +0000

    Passed Test 1 (Check for None Value)

--- Github_Access/Access.py
+++ Github_Access/Access.py
@@ -7,12 +7,15 @@ Returns user projects as a string. Displays:
 - Projects & Toppics
 '''
 def getProjects(user):
+
+    if user is None:
+        return 'No User Provided'
     return ''
 
 #Program==================================================
+if __name__ == '__main__':
+    #Create github account using access token
+    g = Github("a28d927e2e3ed1098f299df8c11868c7ffc290ce")
 
-#Create github account using access token
-g = Github("a28d927e2e3ed1098f299df8c11868c7ffc290ce")
-
-for repo in g.get_user().get_repos():
-    print('Repo: %s, Toppics: %s' % (repo.name, repo.get_topics()))
+    for repo in g.get_user().get_repos():
+        print('Repo: %s, Toppics: %s' % (repo.name, repo.get_topics()))
--- Github_Access/test_Access.py
+++ Github_Access/test_Access.py
@@ -5,7 +5,7 @@ class test_LCA(unittest.TestCase):
 
     def test_getProjects(self):
         #Test 1: Test fuction with None value
-        self.assertEqual(Access.getProjects(None), 'No User provided')
+        self.assertEqual(Access.getProjects(None), 'No User Provided')
 
         #Test 2: Test with valid user
         # TODO test with my account
