33f0612106884ae7e8bce0aef4454a13edd2399f
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-11-11 20:56:27 +0000

    Adding form to home page

--- Github_Access/Access_FrontEnd/User/views.py
+++ Github_Access/Access_FrontEnd/User/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from github import Github
+from .forms import UserForm
 
 class userView(TemplateView):
+    template_name = 'User/userView.html'
+    form_class = UserForm
 
     def get(self, request):
-        return render(request, 'User/userView.html')
+        context = {
+            'form' : self.form_class
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid:
+            print(form.data['usersName'])
+
+        return render(request, self.template_name)
+
