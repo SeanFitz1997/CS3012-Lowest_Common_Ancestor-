186d96d9876810b0aadfff99bad17b8d7aa5d851
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-11-16 15:19:50 +0000

    Bar chart for user totals

--- Github_Access/User/views.py
+++ Github_Access/User/views.py
@@ -7,29 +7,29 @@ from .forms import UserForm
 from Access_API.Access import getLangSkills, getRepoDetails
 
 class userView(TemplateView):
-    template_name = 'User/userView.html'
+    loginTemplate = 'User/login.html'
+    userViewTemplate = 'User/userView.html'
     form_class = UserForm
 
     def get(self, request):
         context = {
             'form' : self.form_class
         }
-        return render(request, self.template_name, context)
+        return render(request, self.loginTemplate, context)
 
     def post(self, request):
         form = self.form_class(request.POST)
         if form.is_valid:
             try:
                 g = Github(form.data['userName'], form.data['password'])
-                lang_info = getLangSkills(g) 
+                #lang_info = getLangSkills(g) 
                 
                 context = {
                     'user_login' : g.get_user(),
-                    'lang_info' : lang_info[0]
+                    'lang_info' : getLangSkills(g)
                 }
-                return render(request, self.template_name, context)
+                return render(request, self.userViewTemplate, context)
             except:
-                messages.add_message(request, messages.ERROR, 'Invalid Github details.')
-            
-        return render(request, self.template_name)
+                messages.add_message(request, messages.ERROR, 'Invalid Github details.')  
+        return render(request, self.loginTemplate)
 
