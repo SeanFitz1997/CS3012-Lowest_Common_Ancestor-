a824b21665fa9af388c7425018dac6dfee79320a
SeanFitz1997 <33702330+SeanFitz1997@users.noreply.github.com>
2018-11-17 15:22:22 +0000

    Adding user repo pie charts

--- Github_Access/User/views.py
+++ Github_Access/User/views.py
@@ -23,9 +23,16 @@ class userView(TemplateView):
             try:
                 g = Github(form.data['userName'], form.data['password'])
                 #lang_info = getLangSkills(g) 
-                
+                response = [{'Name': 'User', 'Total': 7056362, 'JavaScript': 4959458, 'HTML': 243166, 'CSS': 1733424, 'PHP': 2436, 'Python': 79858, 'C': 7626, 'Java': 25736, 'C++': 4658}, {'Name': 'CryptoDonator', 'Total': 2608215, 'JavaScript': 2021614, 'HTML': 108772, 'CSS': 476611, 'PHP': 1218}, {'Name': 'SoftwareEngineeringProject', 'Total': 2608215, 'JavaScript': 2021614, 'HTML': 108772, 'CSS': 476611, 'PHP': 1218}, {'Name': 'CS3012_SoftwareEngineering', 'Total': 867977, 'Python': 18336, 'CSS': 390069, 'JavaScript': 458115, 'HTML': 1457}, {'Name': 'DNS_Validator_API', 'Total': 7626, 'C': 7626}, {'Name': 'EBII1819--Trinity_Module_Review', 'Total': 896728, 'Python': 25270, 'HTML': 23210, 'CSS': 390133, 'JavaScript': 458115}, {'Name': 'ItunesPlaylist_Parser', 'Total': 4275, 'Python': 4275}, {'Name': 'python_machine_learning', 'Total': 31977, 'Python': 31977}, {'Name': 'Snake', 'Total': 26691, 'Java': 25736, 'HTML': 955}, {'Name': 'Socket-GET-request', 'Total': 4658, 'C++': 4658}]
+                user_lang_info = response[0]
+                user_repo_info = response[1:]
+                repo_len = len(user_repo_info)
+
                 context = {
-                    'user_login' : g.get_user()
+                    'user_login' : g.get_user(),
+                    'user_lang_info' : user_lang_info,
+                    'user_repo_info' : user_repo_info,
+                    'range' : range(repo_len)
                 }
                 return render(request, self.userViewTemplate, context)
             except:
